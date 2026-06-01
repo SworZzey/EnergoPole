dc8150e923d4667e22b6afdc74dd55b9f98e91c2
Gleb Zils <zils.gleb@gmail.com>
2026-06-01 23:09:59 +0700

    added sw.js to gitignore

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7fpp36tutbs"
+    "revision": "0.pup9pami8fs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
